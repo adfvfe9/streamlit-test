c3c3e6d4f89420400c39c57c019dbc92b256a47d
공은성 <137687927+adfvfe9@users.noreply.github.com>
2025-08-19 06:10:24 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("공은성의 연습용 스트림릿")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "앙"
 )
